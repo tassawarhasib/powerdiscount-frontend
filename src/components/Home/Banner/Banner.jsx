2b48c4f344c7d0e4df14689e900382d59b0c3c58
Tassawar hasib <nabilhasib840@gmail.com>
2024-02-09 19:03:07 +0530

    Optized section responsiveness

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -6,12 +6,12 @@ const Banner = () => {
     <div className="hero-banner">
       <div className="content">
         <div className="text-content">
-          <h1>SALE!!</h1>
+          <h1>Elevate Your Lifestyle with Premium Accessories</h1>
           <p>
-            Discover the Latest in Accessories at Unbeatable Prices!
+            Discover our selection of high-quality accessories best suited for quality lovers and Gen-Z. Enjoy crystal-clear sound and ergonomic designs with our wide range of products. Shop now and upgrade your experience.
           </p>
           <div className="ctas">
-            <div className="banner-cta">Read More</div>
+            <div className="banner-cta">View Products</div>
             <div className="banner-cta v2">Shop Now</div>
           </div>
         </div>
